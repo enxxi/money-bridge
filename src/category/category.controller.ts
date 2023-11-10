4292a0a5aa76200bde4a72ae98fade1554538433
enxxi <zhes4593@gmail.com>
2023-11-10 23:44:36 +0900

    feat: 카테고리 목록 반환 구현

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common'
+import { CategoryService } from './category.service'
+import { Category } from './entities/category.entity'
+import { AuthGuard } from '@nestjs/passport'
 
-@Controller('category')
-export class CategoryController {}
+@Controller('budgets/category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @UseGuards(AuthGuard())
+  @Get()
+  async getCategoryList(): Promise<Category[]> {
+    return await this.categoryService.getCategoryList()
+  }
+}
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { CategoryController } from './category.controller';
-import { CategoryService } from './category.service';
+import { Module } from '@nestjs/common'
+import { CategoryController } from './category.controller'
+import { CategoryService } from './category.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Category } from './entities/category.entity'
+import { PassportModule } from '@nestjs/passport'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Category]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryService],
 })
 export class CategoryModule {}
